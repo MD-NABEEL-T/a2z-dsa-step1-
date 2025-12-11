975c38ecb698b2fc25d0a91bf073c93298001f40
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-12-11 07:31:56 +0530

    solved problem 1

--- Lecture 1/problem1.java
+++ Lecture 1/problem1.java
@@ -1,5 +1,7 @@
-class Solution {
-    public void printNumber(Scanner sc) {
+import java.util.*;
+public class problem1{
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
         System.out.println(input);
     }
