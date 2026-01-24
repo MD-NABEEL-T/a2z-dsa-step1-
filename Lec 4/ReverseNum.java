4390b774a2033bae75926243f37b4af31a858e5b
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-01-24 13:01:07 +0530

    updated

--- Lec 4/ReverseNum.java
+++ Lec 4/ReverseNum.java
@@ -1,12 +1,26 @@
 public class ReverseNum {
-    public static void main(String []args){
-        int n=9230;
+    public static int reverseNumber(int n){
         int revNum=0,lastDigit=0;
         while(n>0){
             lastDigit=n%10;
             n=n/10;
             revNum=(revNum*10)+lastDigit;
         }
-        System.out.println(revNum);
+        return(revNum);
+    }
+    public static void main(String []args){
+        int n=-439230;
+        int result;
+        if(n<0){
+            n=-1*n;
+            result =reverseNumber(n);
+            result=-1*result;
+        }
+        else{
+            result=reverseNumber(n);
+        }
+
+        System.out.println(result);
     }
 }
+// in leetcode you are converting it to long because of the question and while returning it you typecast it to int.
