dd96eecea9a1db81205fc59b0c99882f3c1d3ae8
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-01-24 11:15:10 +0530

    updated

--- Lec 4/ReverseNumber.java
+++ Lec 4/ReverseNumber.java
@@ -2,7 +2,12 @@ import java.util.*;
 public class ReverseNumber {
     public static void main(String []args){
         int n=234700;//7432
-        int i=n%10;
-        System.out.println(i);
+        int revNum=0;
+        int lastDigit;
+        while(n>0){
+            lastDigit=n%10;
+            n=n/10;
+            revNum=revNum*10+lastDigit;
+        }
     }
 }
