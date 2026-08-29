aaf58978e7e53433f60c77a0a45c091f62890d16
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-08-29 22:55:50 +0530

    fix: Selection sort learnt

--- Step 2/sorting1/ps1.java
+++ Step 2/sorting1/ps1.java
@@ -18,25 +18,52 @@ public class ps1{
 //     }
 // }
 
-    public static void main(String []args){
-        int arr1[]={12,34,5,23};
-        for(int i=0;i<arr1.length-1;i++){
- 
-            //assume minimum initially as 0.
-            int minimum=i;
 
-            //find the minimum by comparing
-            for(int j=i;j<arr1.length-1;j++){
-                if(arr1[i]>arr1[j]){
-                    mimimum=j;
+    // public static void main(String []args){
+    //     int arr1[]={12,34,5,23};
+
+    //     // here len-1 is because oncce the last before number is sorted then the last number is also sorted right that's why 
+    //     for(int i=0;i<arr1.length-2;i++){
+ 
+    //         //assume minimum initially as 0.
+    //         int minimum=i;
+
+    //         //find the minimum by comparing
+    //         for(int j=i;j<arr1.length-1;j++){
+    //             if(arr1[i]>arr1[j]){
+    //                 mimimum=j;
+    //             }
+    //         }
+
+    //         // then swap the minimum value 
+    //         int temp=arr1[i];
+    //         arr1[i]=arr1[j];
+    //         arr1[j]=temp;
+            
+    //     }
+    //     System.out.println(Arrays.toString(arr1));
+    // }
+
+
+    public static void main(String []args){
+        int arr[]={23,1,3,43,32};
+        int min=0;
+        int n=arr.length;
+
+        // n-1 because if before last number is sorted then the last number is also sorted right 
+        for(int i=0;i<n-1;i++){
+            //to move to next when one number is sorted 
+            min=i;
+            for(int j=i;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
                 }
             }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
 
-            // then swap the minimum value 
-            int temp=arr1[i];
-            arr1[i]=arr1[j];
-            arr1[j]=temp;
-            
         }
-        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr));
     }
+}
