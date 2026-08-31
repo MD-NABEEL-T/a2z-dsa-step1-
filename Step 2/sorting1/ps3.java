1efc576a0c4889b729b21117f5e25133a2be0d69
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2026-08-31 22:38:52 +0530

    updated

--- Step 2/sorting1/ps3.java
+++ Step 2/sorting1/ps3.java
@@ -1,10 +1,26 @@
 /**
- * INSERTION SORT - 
+ * INSERTION SORT - compare next index ,sort it and move to the next index and compare itwith the sorted 
  * ps3
  */
+import java.util.*;
 public class ps3 {
 
     public static void main(String[] args) {
-        
+        int arr[]={14,9,15,6,8};
+        int n=arr.length;
+        // int k=0;
+        for(int i=1;i<n;i++){
+            int j=i;
+            while(j>0 && arr[j]<arr[j-1]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
+
+// best case = o(n);
+// worst case=o(n2);
---
+++ Step 2/sorting2/ps4.java
@@ -0,0 +1,48 @@
+
+/**
+ * ps4
+ */
+import java.util.*;
+public class ps4 {
+    static void merge(int arr[],int low,int mid,int high){
+        List<Integer> temp = new ArrayList<>();
+        int left=low;
+        int right=mid+1;
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }
+            else{
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.add(arr[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.add(arr[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            arr[i]=temp.get(i-low);
+        }
+    }
+    static void mergeSort(int arr[],int low, int high){
+        if(low>=high){
+            return;
+        }
+        int mid=(low+high)/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+    public static void main(String[] args) {
+        int arr[]={24,10,8,3,5,1};
+        int n=arr.length;
+        mergeSort(arr,0,n-1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
